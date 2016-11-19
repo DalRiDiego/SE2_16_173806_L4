669b3c9db2418ddb7df573420a0c18c12d7afcda
root <root@localhost.localdomain>
2016-11-19 11:07:43 +0100

    inizializzazione file

---
+++ server.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+
+
+
+app.set('port',(process.env.PORT || 8080)); //ascolta la porta 8080
+
+app.listen(1337,'127.0.0.1');
+console.log("Server running on port 8080...");
