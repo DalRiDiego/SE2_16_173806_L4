6ee4a1c4cafe4a2e7de7a5c6e1a105498396cf9f
root <root@localhost.localdomain>
2016-11-19 11:11:03 +0100

    err

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ var app = express();
 
 app.set('port',(process.env.PORT || 8080)); //ascolta la porta 8080
 
-app.listen(1337,'127.0.0.1');
+app.listen(8080,'127.0.0.1');
 console.log("Server running on port 8080...");
